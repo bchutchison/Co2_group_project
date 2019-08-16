afb2756f5c9e1e6c7863e2eba98ca976439c8037
bchutchison <benhutchison85@gmail.com>
2019-08-16 15:58:13 +0100

    initial slider bar added

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div id="app">
+    <slider></slider>
+  </div>
+</template>
+
+<script>
+import Slider from "@/components/test-slider.vue";
+
+export default {
+  name: "app",
+  components: {
+    'slider': Slider
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/test-slider.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <slider
+      :values="sliderValues"
+      min="0"
+      max="100"
+      raising
+      v-model="slider"
+    ></slider>
+    <!-- remember to set v-model -->
+    {{ slider }}
+  </div>
+</template>
+
+<script>
+  import Slider from "vue-custom-range-slider";
+  // import the styling, css or scss
+  import "vue-custom-range-slider/dist/vue-custom-range-slider.css";
+
+  export default {
+    components: {
+      Slider
+    },
+    data() {
+      return {
+        slider: "0",
+        sliderValues: [
+          {
+            label: "Not at all",
+            value: "0"
+          },
+          {
+            label: "A tiny bit",
+            value: "1"
+          },
+          {
+            label: "Its ok",
+            value: "2"
+          },
+          {
+            label: "Its very good",
+            value: "3"
+          },
+          {
+            label: "Its AMAZING!",
+            value: "4"
+          }
+        ]
+      };
+    }
+  };
+</script>
