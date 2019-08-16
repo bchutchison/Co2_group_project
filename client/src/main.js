cb612eab6709824ef534f948ca01800f573d6099
bchutchison <benhutchison85@gmail.com>
2019-08-16 15:43:19 +0100

    added highchart

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HighchartsVue from 'highcharts-vue'
 
 Vue.config.productionTip = false
+Vue.use(HighchartsVue)
 
 new Vue({
   render: h => h(App),
