895d1e38a65e537c5629920b8fbc29730cf49e0a
bchutchison <benhutchison85@gmail.com>
2019-08-19 13:44:53 +0100

    added slider bars folder

--- client/src/components/Category.vue
+++ client/src/components/Category.vue
@@ -3,19 +3,28 @@
     <ul v-for="category in emissions">
       <li>{{category.category}}</li>
       <div class="question-topic">
-        <ul v-for="data in category.data">
+        <ul v-for="(data, index) in category.data" :key="index" :data="data">
          <li>{{data.name}}</li>
+         <slider-incremental v-if="!data.isSmoothSlider">Test If v-if works</slider-incremental>
+         <slider-smooth v-else>Test If v-if works</slider-smooth>
         </ul>
-
       </div>
     </ul>
   </div>
 </template>
 
 <script>
+import SliderIncremental from './SliderIncremental.vue'
+import SliderSmooth from './SliderSmooth.vue'
+
 export default {
   name: "category",
-  props: ["emissions"]
+  props: ["emissions"],
+  components: {
+    'slider-incremental': SliderIncremental,
+    'slider-smooth': SliderSmooth
+  }
+
 
 }
 
---
+++ client/src/components/SliderSmooth.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="component-wrapper">
+
+    <div class="slider-wrapper">
+      <slider
+      :min="100"
+      :max="200"
+      raising
+      v-model="slider"
+      ></slider>
+    </div>
+
+      <!-- remember to set v-model -->
+    <!-- <div class="output-wrapper">
+      <!- <p>{{ (parseInt(slider) * this.emissions[2].calcValue + parseInt(slider2)).toFixed(2) }}</p> -->
+    <!-- </div> -->
+
+  </div>
+</template>
+
+<script>
+  import Slider from "vue-custom-range-slider";
+  import "vue-custom-range-slider/dist/vue-custom-range-slider.css";
+
+  export default {
+    components: {
+      Slider
+    },
+
+    props: ["emissions"],
+
+    data() {
+      return {
+        emissions: "",
+        slider: "0",
+        sliderValues: [
+          {
+            label: this.emissions[0].sliderValues[0].label,
+            value: this.emissions[0].sliderValues[0].value
+          },
+          {
+            label: this.emissions[0].sliderValues[1].label,
+            value: this.emissions[0].sliderValues[1].value
+          },
+          {
+            label: this.emissions[0].sliderValues[2].label,
+            value: this.emissions[0].sliderValues[2].value
+          },
+          {
+            label: this.emissions[0].sliderValues[3].label,
+            value: this.emissions[0].sliderValues[3].value
+          }
+
+        ],
+
+
+        // slider2Values: [
+        //   {
+        //     label: "low",
+        //     value: 0
+        //   },
+        //   {
+        //     label: "a little",
+        //     value: 23
+        //   },
+        //   {
+        //     label: "Average",
+        //     value: 46
+        //   },
+        //   {
+        //     label: "Above average",
+        //     value: 77
+        //   },
+        //   {
+        //     label: "a lot",
+        //     value: 100
+        //   }
+        // ]
+      };
+    },
+
+  };
+</script>
+
+<style lang="css" >
+
+.component-wrapper {
+ color: red;
+ width: 60%;
+ border: 1px solid black;
+ padding: 20px;
+ margin-right: 50px;
+}
+
+.output-wrapper {
+  border-style: 1px solid black;
+  color: red;
+}
+
+</style>
