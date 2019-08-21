d9e1ce524d0730f25b107491747d9027118312ab
bchutchison <benhutchison85@gmail.com>
2019-08-21 20:45:38 +0100

    settings page form rewind

--- client/src/components/EmissionDetail.vue
+++ client/src/components/EmissionDetail.vue
@@ -3,26 +3,26 @@
     <ul v-for="emission in emissions">
       <li><h3>{{emission.category}}</h3></li>
       <ul v-for="item in emission.data">
-        <form v-on:submit="handleSubmit">
+        <form v-on:submit="handleSubmit" method="put">
           <li>Title: {{item.name}}</li>
           <li>Slider Type: {{item.isSmoothSlider}}</li>
           <ul v-if="item.isSmoothSlider">
             <label for="calcValue">Calculation Value: {{item.sliderValues.calcValue}}</label>
-            <input type="text" name="calcValue" v-model="calcValue" />
+            <input type="text" name="calcValue"/>
             <br>
             <label for="min">Calculation Value: {{item.sliderValues.min}}</label>
-            <input type="text" name="min" v-model="min" />
+            <input type="text" name="min"/>
             <br>
             <label for="max">Maximum Value: {{item.sliderValues.max}}</label>
-            <input type="text" name="max" v-model="max" />
+            <input type="text" name="max"/>
             <br>
             <label for="modifier">Modifer: {{item.sliderValues.modifier}}</label>
-            <input type="text" name="modifier" v-model="modifier" />
+            <input type="text" name="modifier"/>
           </ul>
-            <ul v-else v-for="(value, index) in item.sliderValues" key="index">
-              <label for="sliderLabel">Label: {{value.label}}</label>
-              <label for="sliderValue"> Value: {{value.value}}</label>
-              <input type="text" name="sliderValue" v-model="sliderValue" />
+            <ul v-else v-for="item in item.sliderValues">
+              <label for="sliderLabel">Label: {{item.label}}</label>
+              <label for="sliderValue"> Value: {{item.value}}</label>
+              <input type="text" name="sliderValue"/>
             </ul>
         <input type="submit" name="submit" value="save">
       </form>
@@ -35,16 +35,18 @@
 </template>
 
 <script>
+import FootprintService from '@/services/FootprintService.js'
+
 export default {
   name: 'emission-detail',
   props: ['emissions'],
-  method: {
+  methods: {
     splitString() {
       var str = item.name;
       console.log(str.split('-')[0]);
     },
-    handleSubmit: function(){
-			FootprintService.updateBooking(this.emissions)
+    handleSubmit(){
+			FootprintService.updateBooking(this.emissions.data._id)
     }
   }
 }
