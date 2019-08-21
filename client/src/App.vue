19968c306d67a8431ad1b62ea805991aa7a51f8f
marmalou <marmalou2@gmail.com>
2019-08-21 13:42:50 +0100

    counter styled

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,10 +6,15 @@
         <router-link :to="{ name: 'stats'}">Stats</router-link>
         <router-link :to="{ name: 'settings'}">Settings</router-link>
       </nav>
-    </header> <h5>{{totalC02Value}}kg Co2</h5>
+    </header>
 
-     <h1 class="main-heading">Carbon Calculator</h1>
-    <h2 class="main-heading">Reduce your Carbon Footprint</h2>
+    <div class="counter-wrapper">
+      <div class="">
+        <h1 class="main-heading">Carbon Calculator</h1>
+        <h2 class="main-heading">Reduce your Carbon Footprint</h2>
+      </div>
+      <div class="counter"><h5>{{totalC02Value}}kg Co2</h5></div>
+    </div>
     <router-view id="view":emissions="emissions"></router-view>
 
     <!-- <category v-for="(emission, index) in this.emissions"
@@ -75,18 +80,40 @@ body {
 
 
 
-counter{
-  background-image: url "https://cdn.onlinewebfonts.com/svg/img_541271.png";
+.counter{
+  background-image: url("https://cdn.onlinewebfonts.com/svg/img_541271.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 275px;
+  width: 275px;
   margin-top: 2em;
   /* attachment: fixed; */
   top: 0;
+  left: 825px;
   font-size: 48px;
-  position: sticky;
-  }
+  position: fixed;
+  z-index: 100;
+}
+
+.counter h5 {
+  color: #469120;
+  font-family: Arial;
+}
+
 .main-heading {
   font-size: 48px;
   font-family: arial;
   color:#469120;
   padding-left: 60px;
+}
+
+.counter-wrapper {
+  display: flex;
+  align-items: center;
 }
 </style>
